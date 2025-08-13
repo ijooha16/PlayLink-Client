ee2561166fffe242a3017e99bf93889688401ce6
lk1 <whitecandy777@naver.com>
2025-08-13 13:32:47 +0900

    feat: 로딩 스피너 컴포넌트 디자인 분기처리

--- src/shares/common-components/loading.tsx
+++ src/shares/common-components/loading.tsx
@@ -2,7 +2,11 @@
 
 import { useEffect } from 'react';
 
-export default function Loading() {
+type LoadingType = {
+  variant: 'dim' | 'white';
+};
+
+export default function Loading({ variant = 'dim' }: LoadingType) {
   useEffect(() => {
     document.body.style.overflow = 'hidden';
     return () => {
@@ -10,8 +14,13 @@ export default function Loading() {
     };
   }, []);
 
+  const backgroundColor =
+    variant === 'dim' ? 'bg-[rgba(0,0,0,0.6)]' : 'bg-white';
+
   return (
-    <div className='fixed inset-0 z-50 flex items-center justify-center bg-[rgba(0,0,0,0.2)]'>
+    <div
+      className={`fixed inset-0 z-50 flex items-center justify-center ${backgroundColor}`}
+    >
       <div className='h-8 w-8 animate-spin rounded-full border-2 border-gray-200 border-t-blue-500'></div>
     </div>
   );
