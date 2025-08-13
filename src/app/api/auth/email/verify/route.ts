3a60b6643b80e8520336d7b95968332f2b812b8c
lk1 <whitecandy777@naver.com>
2025-08-13 14:35:59 +0900

    feat: email 인증코드 전송 api error 처리

--- src/app/api/auth/email/verify/route.ts
+++ src/app/api/auth/email/verify/route.ts
@@ -4,26 +4,27 @@ export async function POST(request: Request) {
   try {
     const payload = await request.json();
     const fetchURL = process.env.NEXT_PUBLIC_DB_URL;
-    const backendApiUrl = `${fetchURL}/playlink/signup/email`;
-    console.log(payload);
+    const backendApiUrl = `${fetchURL}/playlink/signup/email/verify`;
 
     const res = await fetch(backendApiUrl, {
       method: 'POST',
-      body: payload,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload),
     });
 
+    const resJson = await res.json();
+
     if (!res.ok) {
-      const resJson = await res.json();
       console.error('server api error', resJson);
       return NextResponse.json({
         status: 'error',
-        message: 'email server api error',
+        message: 'email verify server api error',
       });
     }
 
     return NextResponse.json({
       status: 'success',
-      message: '발송 완료',
+      message: '이메일 인증코드 확인 되었습니다',
     });
   } catch (err) {
     console.error('email route error', err);
