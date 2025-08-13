304019661c8a898fba6d98c7e263de7ecf227072
lk1 <whitecandy777@naver.com>
2025-08-13 14:39:54 +0900

    feat: 회원가입 스텝 1단계 중간작업

--- src/features/play-link/view/sign-up/step1.tsx
+++ src/features/play-link/view/sign-up/step1.tsx
@@ -11,6 +11,17 @@ import Input from '@/shares/common-components/input';
 import EmailOption from '@/features/play-link/view/sign-up/email-option';
 import useSelector from '@/hooks/useSelector';
 
+import { useEmail } from '@/hooks/email/useEmail';
+import { useEmailVerify } from '@/hooks/email/useEmailVerify';
+
+import {
+  TERMS_OF_SERVICE,
+  PRIVACY_POLICY,
+  LOCATION_POLICY,
+  THIRD_PARTY_POLICY,
+  MARKETING_POLICY,
+} from '@/shares/constant/sigin-up-privacy';
+
 const Step1 = ({
   onNext,
   defaultValues,
@@ -33,10 +44,30 @@ const Step1 = ({
   const watched = useWatch({ control });
 
   const { selectorValue, handleGetSelectorValueData } = useSelector();
+  const [emailAuthView, setEmailAuthView] = useState(false);
+  const [smsView, setSmsView] = useState(false);
 
-  useEffect(() => {
-    console.log(selectorValue);
-  }, [selectorValue]);
+  // 인증코드 입력 상태
+  const [emailCode, setEmailCode] = useState('');
+  const [smsCode, setSmsCode] = useState('');
+
+  const { mutate: emailSend, isPending: isSending } = useEmail({
+    onSuccess: () => {
+      setEmailAuthView(true);
+    },
+    onError: (err) => {
+      console.error('로그인 실패:', err.message);
+    },
+  });
+
+  const { mutate: emailVerify, isPending: isVerifying } = useEmailVerify({
+    onSuccess: () => {
+      setEmailAuthView(false);
+    },
+    onError: (err) => {
+      console.error('로그인 실패:', err.message);
+    },
+  });
 
   const isAllRequiredFilled =
     watched.email?.trim() &&
@@ -53,27 +84,93 @@ const Step1 = ({
     onNext(data);
   };
 
+  // 이메일 인증코드 전송
+  const handleSendEmailCode = () => {
+    if (!watched.email?.trim()) return;
+    if (isSending) return;
+    emailSend({ email: watched.email.trim() }); // <- useEmail 훅에서 기대하는 payload에 맞춰주세요
+  };
+  // 이메일 인증코드 확인
+  const handleEmailCodeCheck = () => {
+    if (!watched.email?.trim()) return;
+    if (!emailCode.trim()) return;
+    if (isVerifying) return;
+
+    emailVerify({
+      email: watched.email.trim(),
+      code: emailCode.trim(),
+    }); // <- useEmailVerify 훅의 payload 스펙에 맞춰주세요
+  };
+
+  // 핸드폰 인증코드 전송
+  const handleSmsCodeCheck = () => {
+    try {
+    } catch (err) {}
+  };
+
+  // 핸드폰 인증코드 확인
+  const handleSendSmsCode = () => {
+    try {
+    } catch (err) {}
+  };
+
+  const checkboxClass =
+    'relative h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-400 checked:border-blue-500 checked:bg-blue-500 checked:after:absolute checked:after:inset-0 checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-xs checked:after:font-bold checked:after:text-white checked:after:content-["✓"]';
+  const termsData: any = [
+    { name: 'agreeTerms', label: '이용약관 동의', required: true },
+    { name: 'agreePrivacy', label: '개인정보 처리방침 동의', required: true },
+    { name: 'isOver14', label: '만 14세 이상입니다', required: true },
+    { name: 'agreeLocation', label: '위치 정보 이용 동의', required: true },
+    { name: 'agreeThirdParty', label: '제 3자 정보 제공 동의', required: true },
+    { name: 'agreeMarketing', label: '마케팅 수신 동의', required: false },
+  ];
+
   return (
+    // <div style={{ whiteSpace: 'pre-wrap' }}>{TERMS_OF_SERVICE}</div>
     <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-4'>
       <div className='flex flex-col space-y-2'>
         <label htmlFor='email-id' className='text-lg font-semibold'>
           아이디
         </label>
         <div>
-          <div className='flex'>
-            <Input
-              id='email-id'
-              type='email'
-              variant={'default'}
-              sizes={'md'}
-              placeholder='이메일'
-              {...register('email')}
-            />
-            <EmailOption
-              onChange={handleGetSelectorValueData}
-              selectorValue={selectorValue}
-            />
+          <div className='flex justify-between gap-4'>
+            <div className='relative flex-1'>
+              <input
+                id='email-id'
+                type='email'
+                className='bg-transparnent text-md w-full rounded-lg border border-gray-300 px-4 py-2 text-inherit placeholder-gray-400 focus:outline-none focus:ring-0 disabled:bg-gray-200'
+                placeholder='이메일'
+                {...register('email')}
+              />
+            </div>
+
+            <button
+              onClick={handleSendEmailCode}
+              type='button'
+              className='w-50 box-border cursor-pointer rounded-lg bg-[#E7E9EC] px-4 text-sm text-[#BDC0C6] transition-colors'
+            >
+              인증 받기
+            </button>
           </div>
+
+          {emailAuthView && (
+            <div className='mt-4 flex justify-between gap-4'>
+              <input
+                id='text'
+                type='text'
+                className='bg-transparnent text-md flex-1 rounded-lg border border-gray-300 px-4 py-2 text-inherit placeholder-gray-400 focus:outline-none focus:ring-0 disabled:bg-gray-200'
+                placeholder='이메일 인증번호'
+                onChange={(e) => setEmailCode(e.target.value)}
+              />
+              <button
+                className='w-300 box-border cursor-pointer rounded-lg bg-[#E7E9EC] px-4 text-sm text-[#BDC0C6] transition-colors'
+                onClick={handleEmailCodeCheck}
+                disabled={isVerifying || !emailCode.trim()}
+              >
+                인증 확인
+              </button>
+            </div>
+          )}
           <p className='h-[20px] text-sm text-red-500'>
             {errors.email && errors.email.message}
           </p>
@@ -123,56 +220,154 @@ const Step1 = ({
           </p>
         </div>
       </fieldset>
-
-      <div className='flex flex-col space-y-2'>
+      <div className='flex flex-col'>
         <label htmlFor='tel' className='text-lg font-semibold'>
           휴대폰 번호
         </label>
         <div>
-          <Input
-            id='tel'
-            type='tel'
-            variant={'default'}
-            sizes={'md'}
-            placeholder='휴대폰 번호'
-            {...register('phone')}
-          />
+          <div className='flex justify-between gap-4'>
+            {/* <Input
+              id='tel'
+              type='tel'
+              variant={'default'}
+              sizes={'lg'}
+              placeholder='휴대폰 번호'
+              {...register('phone')}
+            /> */}
+            <input
+              id='tel'
+              type='tel'
+              className='bg-transparnent text-md flex-1 rounded-lg border border-gray-300 px-4 py-2 text-inherit placeholder-gray-400 focus:outline-none focus:ring-0 disabled:bg-gray-200'
+              placeholder='휴대폰 번호'
+              {...register('phone')}
+            />
+            <button
+              className='w-300 box-border cursor-pointer rounded-lg bg-[#E7E9EC] px-4 text-sm text-[#BDC0C6] transition-colors'
+              onClick={handleSendSmsCode}
+            >
+              인증 받기
+            </button>
+          </div>
+          {smsView && (
+            <div className='mt-4 flex justify-between gap-4'>
+              {/* <Input
+                id='tel'
+                type='tel'
+                variant={'default'}
+                sizes={'md'}
+                placeholder='인증번호'
+                {...register('phone')}
+              /> */}
+              <input
+                id='tel'
+                type='tel'
+                className='bg-transparnent text-md flex-1 rounded-lg border border-gray-300 px-4 py-2 text-inherit placeholder-gray-400 focus:outline-none focus:ring-0 disabled:bg-gray-200'
+                placeholder='인증번호'
+                {...register('phone')}
+              />
+              <button
+                className='w-300 box-border cursor-pointer rounded-lg bg-[#E7E9EC] px-4 text-sm text-[#BDC0C6] transition-colors'
+                onClick={handleSmsCodeCheck}
+              >
+                인증 확인
+              </button>
+            </div>
+          )}
           <p className='h-[20px] text-sm text-red-500'>
             {errors.phone && errors.phone.message}
           </p>
         </div>
       </div>
+      {/* <div className='flex flex-col space-y-2'>
+        <div className='flex justify-between'>
+          <label className='flex cursor-pointer items-center gap-2'>
+            <input
+              type='checkbox'
+              {...register('agreeTerms')}
+              className='relative h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-400 checked:border-blue-500 checked:bg-blue-500 checked:after:absolute checked:after:inset-0 checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-xs checked:after:font-bold checked:after:text-white checked:after:content-["✓"]'
+            />
+            <span>이용약관 동의 (필수)</span>
+          </label>
+          <div className='cursor-pointer text-gray-400'>{'>'}</div>
+        </div>
+
+        <div className='flex justify-between'>
+          <label className='flex cursor-pointer items-center gap-2'>
+            <input
+              type='checkbox'
+              {...register('agreePrivacy')}
+              className='relative h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-400 checked:border-blue-500 checked:bg-blue-500 checked:after:absolute checked:after:inset-0 checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-xs checked:after:font-bold checked:after:text-white checked:after:content-["✓"]'
+            />
+            <span>개인정보 처리방침 동의 (필수)</span>
+          </label>
+          <div className='cursor-pointer text-gray-400'>{'>'}</div>
+        </div>
+
+        <div className='flex justify-between'>
+          <label className='flex cursor-pointer items-center gap-2'>
+            <input
+              type='checkbox'
+              className='relative h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-400 checked:border-blue-500 checked:bg-blue-500 checked:after:absolute checked:after:inset-0 checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-xs checked:after:font-bold checked:after:text-white checked:after:content-["✓"]'
+              {...register('isOver14')}
+            />
+            <span>만 14세 이상입니다 (필수)</span>
+          </label>
+          <div className='cursor-pointer text-gray-400'>{'>'}</div>
+        </div>
+
+        <div className='flex justify-between'>
+          <label className='flex cursor-pointer items-center gap-2'>
+            <input
+              type='checkbox'
+              className='relative h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-400 checked:border-blue-500 checked:bg-blue-500 checked:after:absolute checked:after:inset-0 checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-xs checked:after:font-bold checked:after:text-white checked:after:content-["✓"]'
+              {...register('agreeLocation')}
+            />
+            <span>위치 정보 이용 동의 (필수)</span>
+          </label>
+          <div className='cursor-pointer text-gray-400'>{'>'}</div>
+        </div>
+
+        <div className='flex justify-between'>
+          <label className='flex cursor-pointer items-center gap-2'>
+            <input
+              type='checkbox'
+              className='relative h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-400 checked:border-blue-500 checked:bg-blue-500 checked:after:absolute checked:after:inset-0 checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-xs checked:after:font-bold checked:after:text-white checked:after:content-["✓"]'
+              {...register('agreeThirdParty')}
+            />
+            <span>제 3자 정보 제공 동의 (필수)</span>
+          </label>
+          <div className='cursor-pointer text-gray-400'>{'>'}</div>
+        </div>
+
+        <div className='flex justify-between'>
+          <label className='flex cursor-pointer items-center gap-2'>
+            <input
+              type='checkbox'
+              className='relative h-4 w-4 cursor-pointer appearance-none rounded-full border border-gray-400 checked:border-blue-500 checked:bg-blue-500 checked:after:absolute checked:after:inset-0 checked:after:flex checked:after:items-center checked:after:justify-center checked:after:text-xs checked:after:font-bold checked:after:text-white checked:after:content-["✓"]'
+              {...register('agreeMarketing')}
+            />
+            <span>마케팅 수신 동의 (선택)</span>
+          </label>
+          <div className='cursor-pointer text-gray-400'>{'>'}</div>
+        </div>
+      </div> */}
 
       <div className='flex flex-col space-y-2'>
-        <label>
-          <input type='checkbox' {...register('agreeTerms')} /> 이용약관 동의
-          (필수)
-        </label>
-
-        <label>
-          <input type='checkbox' {...register('agreePrivacy')} /> 개인정보
-          처리방침 동의 (필수)
-        </label>
-
-        <label>
-          <input type='checkbox' {...register('isOver14')} /> 만 14세 이상입니다
-          (필수)
-        </label>
-
-        <label>
-          <input type='checkbox' {...register('agreeLocation')} /> 위치 정보
-          이용 동의 (필수)
-        </label>
-
-        <label>
-          <input type='checkbox' {...register('agreeThirdParty')} /> 제 3자 정보
-          제공 동의 (필수)
-        </label>
-
-        <label>
-          <input type='checkbox' {...register('agreeMarketing')} /> 마케팅 수신
-          동의 (선택)
-        </label>
+        {termsData.map((term: any) => (
+          <div key={term.name} className='flex justify-between'>
+            <label className='flex cursor-pointer items-center gap-2'>
+              <input
+                type='checkbox'
+                {...register(term.name)}
+                className={checkboxClass}
+              />
+              <span>
+                {term.label} ({term.required ? '필수' : '선택'})
+              </span>
+            </label>
+            <div className='cursor-pointer text-gray-400'>{'>'}</div>
+          </div>
+        ))}
       </div>
       <div>
         <button
