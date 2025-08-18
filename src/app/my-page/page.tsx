8895ff0cbc2107fb50deecfa94409bed0c54e213
lk1 <whitecandy777@naver.com>
2025-08-18 10:52:42 +0900

    feat: 마이페이지 작업 마무리

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -3,9 +3,28 @@
 import Header from '@/shares/common-components/header';
 import { ChevronRight, Edit3 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  handleRemoveSessionStorage,
+  handleGetSeesionStorage,
+} from '@/shares/libs/utills/web-api';
 
 export default function MyPage() {
+  const router = useRouter();
+  const handleLogout = () => {
+    const data = handleRemoveSessionStorage('PLAYLINK_AUTH');
+
+    if (data.status === 'success') {
+      router.push('/sign-in');
+    }
+  };
+
+  useEffect(() => {
+    const token = handleGetSeesionStorage();
+    if (!token) {
+      router.push('/sign-in');
+    }
+  }, []);
   return (
     <>
       <Header title='마이페이지' />
@@ -16,7 +35,10 @@ export default function MyPage() {
         </div>
       </div>
       <hr className='mb-4 mt-14' />
-      <div className='flex justify-between py-4 font-bold'>
+      <div
+        className='flex cursor-pointer justify-between py-4 font-bold'
+        onClick={handleLogout}
+      >
         로그아웃 <ChevronRight />
       </div>
       {/* <div className='text-sm mt-20 text-center underline text-gray-500 underline-offset-4'>로그아웃</div> */}
