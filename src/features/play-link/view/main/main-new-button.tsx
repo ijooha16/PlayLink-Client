7627e0b2fd2022e17a324a84dfe5ad81f4a46258
Juha Yoon <ijooha16@gmail.com>
2025-09-08 10:12:47 +0900

    style/main-추가 버튼

--- src/features/play-link/view/main/main-new-button.tsx
+++ src/features/play-link/view/main/main-new-button.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const MainNewButton = () => {
   return (
-    <div className='fixed bottom-24 flex w-full max-w-[640px] px-4 pb-4 pt-2'>
+    <div className='fixed bottom-16 flex w-full max-w-[640px] px-4 pb-4 pt-2'>
       <div className='relative ml-auto mr-7 flex w-full justify-end'>
         <Link
           href={'/create-match'}
