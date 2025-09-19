32f8ac7cb23c8283a7fdabe95af176ec6881ddfa
Juha Yoon <ijooha16@gmail.com>
2025-09-19 13:18:04 +0900

    refactor/안쓰는 파일 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,11 @@ export default function Home() {
   const router = useRouter();
   // const {data: notificationData} = useGetNotificationQuery(token);
   const token = useAuthStore((state) => state.token);
+
   //검색 페이지 이동
   useEffect(() => {
     if (keyword || type)
-      router.replace(`/query?keyword=${keyword}&type=${type}`);
+      router.replace(`/authorized/query?keyword=${keyword}&type=${type}`);
   }, [keyword, type, router]);
 
   //알림 수신
